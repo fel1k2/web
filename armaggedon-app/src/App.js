307d5ec6446f0f63f0b11c2f9a30c9f4465b0989
Князев Евгений Дмитиревич <zhen.cnyazeff@yandex.ru>
2023-04-13 00:08:51 +0300

    simple form

---
+++ armaggedon-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {Button} from "./button/button";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="header">Header lvl1</h1>
+      <div>Test div</div>
+      <Button/>
+    </div>
+  );
+}
+
+
+export default App;
